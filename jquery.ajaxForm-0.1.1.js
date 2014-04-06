44bf66f95c4b6fdfed79064b7b279766dbc92bee
Felix Milea-Ciobanu <felixmilea@gmail.com>
2014-04-06 01:26:59 -0600

    included support for select + some refactoring

--- jquery.ajaxForm-0.1.1.js
+++ jquery.ajaxForm-0.1.1.js
@@ -1,9 +1,11 @@
 (function($) {
+	var validElements = ["input:not([type=submit],[type=button])", "textarea", "select"].join();
 
 	$.fn.ajaxSubmit = function(options) {
-		var $form = $(this);        
-		var method = options.method || $form.attr("method");
-		var action = options.action || $form.attr("action");
+		var $form = $(this);
+		options = options || {};
+		var method = options.method || $form.attr("method") || "GET";
+		var action = options.action || $form.attr("action") || document.URL;
 		var data = options.data ||  {};
 		var success = options.success || function(data){  };
 			
@@ -11,7 +13,8 @@
 			$form.children().each(function() {
 				var $child = $(this);
 				var name = $child.attr("name");
-				if ($child.is("input:not([type=submit],[type=button])") || $child.is("textarea") && data[name] == undefined) {
+
+				if(data[name] == undefined && $child.is(validElements)) {
 					data[ name ] = $child.val();
 				}
 			});
